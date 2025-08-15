06d578814422b9e2e592493255f762ab9251f89b
aiostorecirebon <aiostoredigitalmarketing@gmail.com>
2025-08-15 14:59:11 +0700

    new

--- my-cms/src/api/artikel/controllers/artikel.ts
+++
@@ -1,7 +0,0 @@
-/**
- * artikel controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::artikel.artikel');
--- my-cms/src/api/artikel/services/artikel.ts
+++
@@ -1,7 +0,0 @@
-/**
- * artikel service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::artikel.artikel');
